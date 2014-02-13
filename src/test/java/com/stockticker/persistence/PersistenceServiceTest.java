7f00b54975be3608a4ed719112ccfaad5b0cac09
sconnall <stuart.connall@gmail.com>
2014-02-12 21:31:17 -0500

    added junit tests for PersistenceService

---
+++ src/test/java/com/stockticker/persistence/PersistenceServiceTest.java
@@ -0,0 +1,47 @@
+package com.stockticker.persistence;
+
+import org.junit.Before;
+import org.junit.Test;
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * PersistenceService junit test case.
+ *
+ * @author  Stuart Connall
+ * @version 1.0 02/12/2014
+ */
+
+public class PersistenceServiceTest {
+
+    /**
+     * Sets up each test before they run
+     */
+    @Before
+    public void setUp() {
+
+    }
+
+    /**
+     * Tests the getWatchedStocks method for a value.
+     */
+    @Test
+    public void getWatchedStocksTest() {
+
+    }
+
+    /**
+     * Tests the setStockToWatch(String) method
+     */
+    @Test
+    public void setStockToWatchStringTest() {
+
+    }
+
+    /**
+     * Tests the setStockToWatch(List) method
+     */
+    @Test
+    public void setStockToWatchListTest() {
+
+    }
+}
