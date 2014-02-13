94bbeeae7d28a26687de894008e2ab17688e5335
sconnall <stuart.connall@gmail.com>
2014-02-12 21:28:08 -0500

    added PersistenceService interface

---
+++ src/main/java/com/stockticker/persistence/PersistenceService.java
@@ -0,0 +1,7 @@
+package com.stockticker.persistence;
+
+public class PersistenceService {
+    public List<String> getWatchedStocks(User);
+    public void setStockToWatch(String);
+    public void setStockToWatch(List<String>);
+}
