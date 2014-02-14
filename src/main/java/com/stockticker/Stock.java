c061ff253d7363ac7bc8604f785fe9dc6ed2a478
Mike Grissom <mikeyman77@gmail.com>
2014-02-14 13:13:08 -0800

    Update data classes
    add loggedIn filed to User

--- src/main/java/com/stockticker/Stock.java
+++ src/main/java/com/stockticker/Stock.java
@@ -3,6 +3,7 @@ package com.stockticker;
 import java.util.Date;
 
 public class Stock {
+
     private String symbol;
     private Date date;
 
--- src/main/java/com/stockticker/User.java
+++ src/main/java/com/stockticker/User.java
@@ -1,7 +1,9 @@
 package com.stockticker;
 
 public class User {
+
     private String userName;
+    private boolean loggedIn;
 
     public User(String userName) {
         this.userName = userName;
