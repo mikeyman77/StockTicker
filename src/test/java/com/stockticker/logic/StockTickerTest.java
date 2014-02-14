335a4baa1ee95fd8ce145dad26aa580e718a3267
Mike Grissom <mikeyman77@gmail.com>
2014-02-14 13:49:06 -0800

    Add test stubs for StockTicker and UserAuthorization

---
+++ src/test/java/com/stockticker/logic/StockTickerTest.java
@@ -0,0 +1,30 @@
+package com.stockticker.logic;
+
+import org.junit.Test;
+
+public class StockTickerTest {
+    @Test
+    public void testGetStockQuote() throws Exception {
+
+    }
+
+    @Test
+    public void testGetStockQuotes() throws Exception {
+
+    }
+
+    @Test
+    public void testGetTrackedStocks() throws Exception {
+
+    }
+
+    @Test
+    public void testTrackStock() throws Exception {
+
+    }
+
+    @Test
+    public void testIsStockTracked() throws Exception {
+
+    }
+}
---
+++ src/test/java/com/stockticker/logic/UserAuthorizationTest.java
@@ -0,0 +1,30 @@
+package com.stockticker.logic;
+
+import org.junit.Test;
+
+public class UserAuthorizationTest {
+    @Test
+    public void testLogIn() throws Exception {
+
+    }
+
+    @Test
+    public void testLogOut() throws Exception {
+
+    }
+
+    @Test
+    public void testRegister() throws Exception {
+
+    }
+
+    @Test
+    public void testUnRegister() throws Exception {
+
+    }
+
+    @Test
+    public void testIsLoggedIn() throws Exception {
+
+    }
+}
