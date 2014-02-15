f438679cd0e628f3d3f11fa05ba4ca55d6a4d152
sconnall <stuart.connall@gmail.com>
2014-02-14 19:33:23 -0500

    Added the @Override annotation on all interface implementation methods

--- src/main/java/com/stockticker/persistence/StockTickerPersistence.java
+++ src/main/java/com/stockticker/persistence/StockTickerPersistence.java
@@ -1,31 +1,38 @@
 package com.stockticker.persistence;
 
 import java.util.List;
-import com.stockticker.*;
+import com.stockticker.Stock;
+import com.stockticker.User;
 
 public enum StockTickerPersistence implements PersistenceService {
     INSTANCE;
 
+    @Override
     public List<Stock> getTrackedStocks(User user) {
         return (List<Stock>)null;
     }
 
+    @Override
     public boolean trackStock(User user, Stock stock, boolean track) {
         return false;
     }
 
+    @Override
     public boolean isStockTracked(User user, Stock stock) {
         return false;
     }
 
+    @Override
     public boolean saveUser(User user) {
         return false;
     }
 
+    @Override
     public boolean loadUser(User user) {
         return false;
     }
 
+    @Override
     public boolean deleteUser(User user) {
         return false;
     }
