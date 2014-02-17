5ecf9e53ecdde40b9b8589ad1fe439ff3f8ebd24
sconnall <stuart.connall@gmail.com>
2014-02-17 04:06:55 -0500

    Added additional tests for PersistenceService interface methods

--- src/test/java/com/stockticker/persistence/PersistenceServiceTest.java
+++ src/test/java/com/stockticker/persistence/PersistenceServiceTest.java
@@ -1,8 +1,14 @@
 package com.stockticker.persistence;
 
+import java.util.List;
+import com.stockticker.Stock;
+import com.stockticker.User;
 import org.junit.Before;
 import org.junit.Test;
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
 
 /**
  * PersistenceService junit test case.
@@ -13,12 +19,32 @@ import static junit.framework.Assert.assertEquals;
 
 public class PersistenceServiceTest {
 
+    private PersistenceService persistence;
+    private User pedroia;
+    private User ortiz;
+    private User victorino;
+    private Stock google;
+    private Stock apple;
+    private Stock microsoft;
+
     /**
      * Sets up each test before they run
      */
     @Before
     public void setUp() {
-
+        persistence = StockTickerPersistence.INSTANCE;
+        pedroia = new User("Pedroia");
+        ortiz   = new User("Ortiz");
+        victorino = new User("Victorino");
+        persistence.saveUser(pedroia);
+        persistence.saveUser(ortiz);
+        persistence.saveUser(victorino);
+        google = new Stock("GOOG");
+        apple = new Stock("AAPPL");
+        microsoft = new Stock("MSFT");
+        persistence.trackStock(ortiz, google, true);
+        persistence.trackStock(ortiz, apple, true);
+        persistence.trackStock(ortiz, microsoft, true);
     }
 
     /**
@@ -26,7 +52,8 @@ public class PersistenceServiceTest {
      */
     @Test
     public void testGetTrackedStocks() {
-
+        List<Stock> stocks = persistence.getTrackedStocks(ortiz);
+        assertTrue("tracked stocks", (stocks.size() > 0));
     }
 
     /**
@@ -34,23 +61,41 @@ public class PersistenceServiceTest {
      */
     @Test
     public void testTrackStock() {
-
+        persistence.trackStock(ortiz, google, true);
+        assertTrue("stock tracked", persistence.isStockTracked(ortiz, google));
     }
 
     /**
-     * Tests the isStockTracked method
+     * Tests the isStockTracked method is true
      */
     @Test
-    public void testIsStockTracked() {
-
+    public void testIsStockTrackedTrue() {
+        persistence.trackStock(ortiz, google, true);
+        assertTrue("stock tracked", persistence.isStockTracked(ortiz, google));
     }
 
     /**
-     * Tests the userExists method
+     * Tests the isStockTracked method is false
      */
     @Test
-    public void testUserExists() {
+    public void testIsStockTrackedFalse() {
+        assertFalse("stock tracked", persistence.isStockTracked(ortiz, new Stock("JAVA")));
+    }
 
+    /**
+     * Tests the userExists method is true
+     */
+    @Test
+    public void testUserExistsTrue() {
+        assertTrue("user exists", persistence.userExists(ortiz));
+    }
+
+    /**
+     * Tests the userExists method is false
+     */
+    @Test
+    public void testUserExistsFalse() {
+        assertFalse("user doesn't exist", persistence.userExists(new User("Connall")));
     }
 
     /**
@@ -58,7 +103,9 @@ public class PersistenceServiceTest {
      */
     @Test
     public void testSaveUser() {
-        
+        User brady = new User("Brady");
+        persistence.saveUser(brady);
+        assertTrue("save user", persistence.userExists(brady));
     }
 
     /**
@@ -66,7 +113,7 @@ public class PersistenceServiceTest {
      */
     @Test
     public void testLoadUser() {
-
+        assertNotNull("load user", persistence.loadUser(victorino));
     }
 
     /**
@@ -74,7 +121,9 @@ public class PersistenceServiceTest {
      */
     @Test
     public void testDeleteUser() {
-
+        User manning = new User("Manning");
+        persistence.saveUser(manning);
+        assertTrue("delete user", persistence.deleteUser(manning));
     }
 
 }
