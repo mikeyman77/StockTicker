27095f3b588b3998cb9dc1da36e56fe471155fdb
sconnall <stuart.connall@gmail.com>
2014-02-17 04:04:18 -0500

    added stub code for interface methods

--- src/main/java/com/stockticker/persistence/StockTickerPersistence.java
+++ src/main/java/com/stockticker/persistence/StockTickerPersistence.java
@@ -1,46 +1,71 @@
 package com.stockticker.persistence;
 
+import java.util.ArrayList;
 import java.util.List;
 import com.stockticker.Stock;
 import com.stockticker.User;
 
 public enum StockTickerPersistence implements PersistenceService {
     INSTANCE;
-    
-    private String name;
+
+    private List<Stock> stocks = new ArrayList<Stock>();
+    private List<User> users = new ArrayList<User>();
 
     @Override
     public List<Stock> getTrackedStocks(User user) {
-        return null;
+        return stocks;
     }
 
     @Override
     public boolean trackStock(User user, Stock stock, boolean track) {
-        return false;
+        if (!stocks.contains(stock))
+            stocks.add(stock);
+        return true;
     }
 
     @Override
     public boolean isStockTracked(User user, Stock stock) {
-        return false;
+        if (stocks.contains(stock))
+            return true;
+        else
+            return false;
     }
 
     @Override
     public boolean userExists(User user) {
-        return false;
+        if (users.contains(user))
+            return true;
+        else
+            return false;
     }
 
     @Override
     public boolean saveUser(User user) {
-        return false;
+        int i = users.indexOf(user);
+        if (i >= 0)
+            users.set(i, user);
+        else
+            users.add(user);
+
+        return true;
     }
 
     @Override
-    public boolean loadUser(User user) {
-        return false;
+    public User loadUser(User user) {
+        int i = users.indexOf(user);
+        if (i >= 0)
+            return users.get(i);
+        else
+            return null;
     }
 
     @Override
     public boolean deleteUser(User user) {
+        int i = users.indexOf(user);
+        if (i >= 0) {
+            users.remove(i);
+            return true;
+        }
         return false;
     }
 
