980e800c07547303c195a84416d47700f43bdba0
Mike Grissom <mikeyman77@gmail.com>
2014-02-18 14:30:21 -0800

    Add userID field to User

--- src/main/java/com/stockticker/User.java
+++ src/main/java/com/stockticker/User.java
@@ -5,12 +5,14 @@ public class User {
     private String userName;
     private String password;
     private boolean loggedIn;
+    private int userID;
 
     public User(String userName, String password) {
         this.userName = userName;
         this.password = password;
     }
 
+    // Getter methods
     public String getUserName() {
         return this.userName;
     }
@@ -19,4 +21,12 @@ public class User {
         return this.password;
     }
 
+    public boolean isLoggedIn() { return this.loggedIn; }
+
+    public int getUserID() { return this.userID; }
+
+    // Setter methods
+    public void setLoggedIn(boolean loggedIn) { this.loggedIn = loggedIn; }
+
+    public void setUserID(int id) { this.userID = id; }
 }
