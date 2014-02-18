18faf26833409be48e220279533fab74a1106b3d
Mike Grissom <mikeyman77@gmail.com>
2014-02-17 21:26:10 -0800

    Add password to User

--- src/main/java/com/stockticker/User.java
+++ src/main/java/com/stockticker/User.java
@@ -3,10 +3,12 @@ package com.stockticker;
 public class User {
 
     private String userName;
+    private String password;
     private boolean loggedIn;
 
-    public User(String userName) {
+    public User(String userName, String password) {
         this.userName = userName;
+        this.password = password;
     }
 
     public String getUserName() {
