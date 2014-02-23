46f9b9ae658e673e7f0f56665314ced5aa7f29de
Mike Grissom <mikeyman77@gmail.com>
2014-02-23 14:55:32 -0800

    add user auth implementation code

--- src/main/java/com/stockticker/logic/UserAuthorization.java
+++ src/main/java/com/stockticker/logic/UserAuthorization.java
@@ -1,38 +1,93 @@
 package com.stockticker.logic;
 
+import com.stockticker.User;
 import com.stockticker.UserInfo;
+import com.stockticker.persistence.PersistenceService;
+import com.stockticker.persistence.StockTickerPersistence;
+import java.util.List;
 
 public enum UserAuthorization implements AuthorizationService {
     INSTANCE;
 
+    private PersistenceService persistence = StockTickerPersistence.INSTANCE;
+
     @Override
     public boolean logIn(String username, String password) {
-        return false;
+        User tempUser = new User(username, password);
+        User user = null;
+        boolean successful = false;
+        
+        // check if user exists
+        if (persistence.userExists(tempUser))
+            user = persistence.loadUser(tempUser);
+        
+        else
+            return successful;
+        
+        // get all logged in users
+        List<String> loggedInUsers = persistence.getLoggedInUsers();
+        
+        // log out all users but the username
+        if (loggedInUsers != null && !loggedInUsers.isEmpty()) {
+            for (String uname : loggedInUsers) {
+                if (uname != username)
+                    persistence.setLoginStatus(uname, false);
+            }
+        }
+        
+        // match password
+        if (user !=null && password == user.getPassword()) {
+            user.setLoggedIn(true);
+            persistence.setLoginStatus(user.getUserName(), true);
+            successful = true;
+        }
+        
+        return successful;
     }
 
     @Override
     public boolean logOut(String username) {
-        return false;
+        boolean successful = false;
+        List<String> loggedInUsers = persistence.getLoggedInUsers();
+        
+        if (loggedInUsers != null && !loggedInUsers.isEmpty()) {
+            if (loggedInUsers.contains(username)) {
+                successful = persistence.setLoginStatus(username, false);
+            }
+        }
+        
+        return successful;
     }
 
     @Override
     public boolean isLoggedIn(String username) {
-        return false;
+        return persistence.isLoggedIn(username);
     }
 
     @Override
     public boolean register(String username, String password) {
+        
+        if (persistence.createUser(new User(username, password)) != null) {
+            return true;
+        }
+
         return false;
     }
 
     @Override
     public boolean unRegister(String username) {
+        User user = new User(username, "");
+        
+        if (persistence.userExists(user)) {
+            return persistence.deleteUser(user);
+        }
+        
         return false;
     }
 
     @Override
     public boolean isRegistered(String username) {
-        return false;
+        return persistence.userExists(new User(username, ""));
     }
 
     @Override
@@ -42,6 +97,7 @@ public enum UserAuthorization implements AuthorizationService {
 
     @Override
     public boolean changePassword(String username, String newPassword) {
+        // going to change the interface for this
         return false;
     }
 }
