bb99abf4da950a78e226a986301305d6e763432b
sconnall <stuart.connall@gmail.com>
2014-02-23 17:57:07 -0500

    Fixed incorrect package name from main to com.stockticker.ui

--- src/main/java/com/stockticker/ui/StockTicker_Main.java
+++ src/main/java/com/stockticker/ui/StockTicker_Main.java
@@ -1,10 +1,9 @@
+package com.stockticker.ui;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-package main;
 
 /**
  *
