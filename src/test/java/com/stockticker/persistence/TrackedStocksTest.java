f382713067205cdbb9b707935e0c5d5757f8e3b2
sconnall <stuart.connall@gmail.com>
2014-02-24 01:20:14 -0500

    Added junit tests for TrackedStocks class

--- src/test/java/com/stockticker/persistence/TrackedStocksTest.java
+++ src/test/java/com/stockticker/persistence/TrackedStocksTest.java
@@ -1,7 +1,69 @@
 package com.stockticker.persistence;
 
+import com.stockticker.Stock;
+import org.junit.Before;
+import org.junit.Test;
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNull;
+import static junit.framework.Assert.assertNotNull;
+
 /**
- * Created by AREIL on 2/23/14.
+ * Tests the TrackedStocks class
+ *
+ * @author Stuart Connall
  */
 public class TrackedStocksTest {
+
+    private TrackedStocks tracked;
+
+    private static final String USER1 = "sconnall";
+    private static final Stock GOOG = new Stock("GOOG");
+    private static final Stock AAPL = new Stock("AAPL");
+    private static final Stock MSFT = new Stock("MSFT");
+    private static final Stock ORCL = new Stock("ORCL");
+
+    /**
+     * Sets up the environment by instantiating the
+     * TrackedStock class and adding two Stocks.
+     */
+    @Before
+    public void setUp() {
+        tracked = new TrackedStocks(USER1);
+        tracked.put(GOOG);
+        tracked.put(AAPL);
+    }
+
+    /**
+     * Tests the TrackedStocks put for true
+     */
+    @Test
+    public void testPutTrue() {
+        assertTrue("put stock true", tracked.put(MSFT));
+    }
+
+    /**
+     * Tests the TrackedStocks put for false
+     */
+    @Test
+    public void testPutFalse() {
+        assertFalse("put stock false", tracked.put(null));
+    }
+
+    /**
+     * Tests the TrackedStocks remove for removal of Stock
+     */
+    @Test
+    public void testRemoveNotNull() {
+        assertNotNull("remove Stock not null", tracked.remove(GOOG));
+    }
+
+    /**
+     * Tests the TrackedStocks for failure to remove stock, most
+     * likely because it doesn't exist or is null
+     */
+    @Test
+    public void testRemoveNull() {
+        assertNull("remove Stock null", tracked.remove(ORCL));
+    }
 }
