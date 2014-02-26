72de3b9cb3cdbb79c199b7d5c09b2efc7b5dac71
Mike Grissom <mikeyman77@gmail.com>
2014-02-26 02:02:04 -0800

    revert UserDAO and remove UserDAOImpl

--- src/main/java/com/stockticker/persistence/UserDAO.java
+++ src/main/java/com/stockticker/persistence/UserDAO.java
@@ -1,7 +1,15 @@
 package com.stockticker.persistence;
 
+import com.stockticker.User;
+
 /**
- * Created by AREIL on 2/25/14.
+ * Created by stu on 2/17/14.
  */
 public interface UserDAO {
+
+    public boolean save(User user);
+    public boolean update(User user);
+    public boolean findByUserId(int userId);
+    public User    get(User user);
+    public boolean delete(User user);
 }
