aabc6ae884d391111b0f6a04213b9e95c43ed90f
Mike Grissom <mikeyman77@gmail.com>
2014-02-26 00:33:30 -0800

    add exceptions to the getStockQuotes method

--- src/main/java/com/stockticker/logic/StockTickerService.java
+++ src/main/java/com/stockticker/logic/StockTickerService.java
@@ -1,12 +1,15 @@
 package com.stockticker.logic;
 
 import com.stockticker.StockQuote;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 import java.util.List;
 
 public interface StockTickerService {
 
-    public List<StockQuote> getStockQuotes(List<String> symbols);
+    public List<StockQuote> getStockQuotes(List<String> symbols) 
+            throws MalformedURLException, IOException;
     public List<String> getTrackedStocks(String username);
     public boolean trackStock(String username, String symbol, boolean tracked);
     public boolean isStockTracked(String username, String symbol);
