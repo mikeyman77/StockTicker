96d5d6676bc8dd54170f5acd1f462f87a19c2436
Mike Grissom <mikeyman77@gmail.com>
2014-02-26 03:41:34 -0800

    implement junit test for getStockQuotes

--- src/test/java/com/stockticker/logic/StockTickerTest.java
+++ src/test/java/com/stockticker/logic/StockTickerTest.java
@@ -1,30 +1,34 @@
 package com.stockticker.logic;
 
+import com.stockticker.StockQuote;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertEquals;
+
 public class StockTickerTest {
-    @Test
-    public void testGetStockQuote() throws Exception {
-
-    }
-
+    
+    private final StockTickerService sts = StockTicker.INSTANCE;
+    
+    /**
+     * Test of getStockQuotes method using one valid symbol.
+    */
     @Test
     public void testGetStockQuotes() throws Exception {
-
+        List<String> symbols = new ArrayList<>();
+        List<StockQuote> stockQuotes = new ArrayList<>();
+        
+        symbols.add("YHOO");
+        
+        stockQuotes = sts.getStockQuotes(symbols);
+        
+        int expected = 1;
+        int result = stockQuotes.size();
+        
+        assertEquals(result, expected);
     }
-
-    @Test
-    public void testGetTrackedStocks() throws Exception {
-
-    }
-
-    @Test
-    public void testTrackStock() throws Exception {
-
-    }
-
-    @Test
-    public void testIsStockTracked() throws Exception {
-
-    }
+    
 }
