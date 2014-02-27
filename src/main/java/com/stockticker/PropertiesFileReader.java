83c960774c40f2e48dc60837a7ee71066534e6b6
sconnall <stuart.connall@gmail.com>
2014-02-27 02:41:52 -0500

    Added utility class and junit tests to read properties files

--- src/main/java/com/stockticker/PropertiesFileReader.java
+++ src/main/java/com/stockticker/PropertiesFileReader.java
@@ -1,7 +1,63 @@
 package com.stockticker;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+
 /**
- * Created by AREIL on 2/26/14.
+ * Reads a properties file from disk and loads into a Properties object
+ *
+ * @author Stuart Connall
+ * @version 1.0 02/26/2014
  */
 public class PropertiesFileReader {
+
+    private Properties properties;
+
+    /**
+     * Constructs a Properties object using the contents
+     * of filename argument.
+     *
+     * @param filename
+     */
+    public PropertiesFileReader(String filename) {
+        try {
+            FileReader reader = new FileReader(filename);
+            properties = new Properties();
+            properties.load(reader);
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    /**
+     * Returns an enumuration of property keys
+     *
+     * @return property keys
+     */
+    public Enumeration<?> getPropertyNames() {
+        return properties.propertyNames();
+    }
+
+    /**
+     * Gets a property value by key
+     *
+     * @param property key
+     * @return property value
+     */
+    public String getProperty(String property) {
+        return properties.getProperty(property);
+    }
+
+    public static void main(String [] args) {
+        String key = null;
+        PropertiesFileReader properties = new PropertiesFileReader("./config/stockticker.properties");
+        for (Enumeration<?> property = properties.getPropertyNames(); property.hasMoreElements();) {
+            key = (String) property.nextElement();
+            System.out.println(key+"="+properties.getProperty(key));
+        }
+
+    }
 }
---
+++ src/test/java/com/stockticker/PropertiesFileReaderTest.java
@@ -0,0 +1,92 @@
+package com.stockticker;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.io.IOException;
+
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
+
+/**
+ * JUnit tests for the PropertiesFileReader class
+ *
+ * @author Stuart Connall
+ * @version 1.0 - 2/26/14.
+ */
+public class PropertiesFileReaderTest {
+
+    private PropertiesFileReader properties;
+    private static final String PROP_FILE = "./config/stockticker.properties";
+    private static final String DUMMY_PROP_FILE = "./config/junittest.properties";
+
+    /**
+     * Constructs PropertiesFileReader using stockticker.properties file
+     */
+    public PropertiesFileReaderTest() {
+        properties = new PropertiesFileReader(PROP_FILE);
+    }
+
+    /**
+     * Tests that the constructor is successfully executed
+     */
+    @Test
+    public void testPropertiesFileReaderConstructionValid() {
+        assertNotNull("valid properties file", properties);
+    }
+
+    /**
+     * Defines a rule for expected exceptions
+     */
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
+
+    /**
+     * Tests that an IOException occurred in the constructor
+     */
+    @Test
+    public void testPropertiesFileReaderConstructionGetsIOException() {
+
+        exception.expect(IOException.class);
+        //exception.expectMessage("invalid properties file");
+        PropertiesFileReader properties = new PropertiesFileReader("");
+    }
+
+    /**
+     * Tests that the getPropertyNames method returns a non-empty enumeration
+     */
+    @Test
+    public void testGetPropertyNames() {
+        assertTrue("get property names", properties.getPropertyNames().hasMoreElements());
+    }
+
+    /**
+     * Tests that the getPropertyNames method returns a non-empty enumeration
+     */
+    @Test
+    public void testGetPropertyNamesEmpty() {
+        PropertiesFileReader dummy = new PropertiesFileReader(DUMMY_PROP_FILE);
+        assertFalse("no property names exist", dummy.getPropertyNames().hasMoreElements());
+    }
+
+    /**
+     * Tests that the getProperty method returns a value
+     */
+    @Test
+    public void testGetPropertyNotNull() {
+        assertNotNull("get property success", properties.getProperty("dbName"));
+    }
+
+    /**
+     * Tests that the getPropertyNames method returns a non-empty enumeration
+     */
+    @Test
+    public void testGetPropertyNull() {
+        assertNull("get property fail", properties.getProperty("noSuchThing"));
+    }
+
+}
