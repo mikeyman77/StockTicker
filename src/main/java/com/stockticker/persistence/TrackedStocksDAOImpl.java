aa2c977dac8ede17309ec59bbba775de3f1e4bcf
sconnall <stuart.connall@gmail.com>
2014-02-27 23:46:22 -0500

    Added JavaDoc and fixed some minor bugs discovered while testing

--- src/main/java/com/stockticker/persistence/TrackedStocksDAOImpl.java
+++ src/main/java/com/stockticker/persistence/TrackedStocksDAOImpl.java
@@ -49,22 +49,44 @@ public class TrackedStocksDAOImpl implements TrackedStocksDAO {
         return -1;
     }
 
+    /**
+     * Adds a row to the tracked stocks table with userId and stockId
+     *
+     * @param userId  the id associated with the user row
+     * @param stockId the id associated with the stock row
+     * @return true if added, false otherwise
+     */
     @Override
     public boolean add(int userId, int stockId) {
-        try {
-            //if trackId returned, then stock is already being tracked
-            PreparedStatement prepared = connection.prepareStatement("INSERT INTO tracked_stock (userId, stockId) VALUES (?,?)");
-            prepared.setInt(1, userId);
-            prepared.setInt(2, stockId);
-            prepared.execute();
-            //trackId = prepared.getGeneratedKeys();
+        if (!exists(userId, stockId)
+            && isValidId(userId)
+            && isValidId(stockId)) {
+
+            try {
+                //if trackId returned, then stock is already being tracked
+                PreparedStatement prepared = connection.prepareStatement("INSERT INTO tracked_stock (userId, stockId) VALUES (?,?)");
+                prepared.setInt(1, userId);
+                prepared.setInt(2, stockId);
+                prepared.execute();
+                //trackId = prepared.getGeneratedKeys();
+            }
+            catch(SQLException e) {
+                System.out.println(e.getMessage());
+            }
+            return true;
         }
-        catch(SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return true;
+
+        return false;
     }
 
+    /**
+     * Tests if the row matching the userId and stockId exists in the
+     * tracked stocks table
+     *
+     * @param userId  the id associated with the user row
+     * @param stockId the id associated with the stock row
+     * @return true if exists, false otherwise
+     */
     @Override
     public boolean exists(int userId, int stockId) {
         try {
@@ -82,6 +104,12 @@ public class TrackedStocksDAOImpl implements TrackedStocksDAO {
         return false;
     }
 
+    /**
+     * Retrieves all tracked stocks associated with the userId
+     *
+     * @param  userId the id associated with the user row
+     * @return a list of stocks or an empty list
+     */
     @Override
     public List<String> get(int userId) {
         List<String> trackedStocks = new ArrayList<String>();
@@ -101,6 +129,14 @@ public class TrackedStocksDAOImpl implements TrackedStocksDAO {
         return trackedStocks;
     }
 
+    /**
+     * Deletes the row from the tracked stocks table associated
+     * with the userId and stockId
+     *
+     * @param userId  the id associated with the user row
+     * @param stockId the id associated with the stock row
+     * @return true if successful, false otherwise
+     */
     @Override
     public boolean delete(int userId, int stockId) {
         try {
@@ -110,16 +146,22 @@ public class TrackedStocksDAOImpl implements TrackedStocksDAO {
 
             prepared.setInt(1, userId);
             prepared.setInt(2, stockId);
-            prepared.executeUpdate();
+            if (prepared.executeUpdate() == 0)
+                return false;
         }
         catch(SQLException e) {
             System.out.println(e.getMessage());
-            return false;
         }
 
         return true;
     }
 
+    /**
+     * Deletes all tracked stocks associated with the userId
+     *
+     * @param  userId the id associated with the user
+     * @return true if successful, false otherwise
+     */
     @Override
     public boolean deleteAll(int userId) {
         try {
@@ -128,14 +170,18 @@ public class TrackedStocksDAOImpl implements TrackedStocksDAO {
                     ("DELETE FROM tracked_stock WHERE userId = ?");
 
             prepared.setInt(1, userId);
-            prepared.executeUpdate();
+            if (prepared.executeUpdate() == 0)
+                return false;
         }
         catch(SQLException e) {
             System.out.println(e.getMessage());
-            return false;
         }
 
         return true;
+    }
+
+    private boolean isValidId(int id) {
+        return (id > 0) ? true : false;
     }
 
 }
