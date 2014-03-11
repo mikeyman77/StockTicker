f210a1160b6aae442e764a666ef7e062ea36d06e
Mike Grissom <mikeyman77@gmail.com>
2014-03-10 21:10:09 -0700

    update admin gui form

--- src/main/java/com/stockticker/admin/Admin.java
+++ src/main/java/com/stockticker/admin/Admin.java
@@ -1,7 +1,11 @@
 package com.stockticker.admin;
 
+import javax.swing.*;
+
 /**
  * Created by Michael on 3/10/14.
  */
 public class Admin {
+    private JTabbedPane tabbedPane1;
+    private JPanel panel1;
 }
