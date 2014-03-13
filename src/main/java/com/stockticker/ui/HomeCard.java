0691c2a7733aa80b632a81d4dce9d5cc5f807d84
Hunter97 <paul.wallace@hypertherm.com>
2014-03-12 22:50:52 -0400

    Stock List item 1, General item 1,

--- src/main/java/com/stockticker/ui/HomeCard.java
+++ src/main/java/com/stockticker/ui/HomeCard.java
@@ -11,13 +11,13 @@ import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-//import java.awt.image.BufferedImage;
-//import java.io.File;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
-//import javax.imageio.ImageIO;
+import javax.imageio.ImageIO;
 
 import javax.swing.BorderFactory;
-//import javax.swing.ImageIcon;
+import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
@@ -29,7 +29,7 @@ import javax.swing.border.EtchedBorder;
 public final class HomeCard extends JPanel {
     private static final long serialVersionUID = 1L;
     private JPanel m_homeCard;
-    //private BufferedImage m_splashImage;
+    private BufferedImage m_splashImage;
     private final GridBagConstraints m_constraints;
 
 
@@ -49,26 +49,31 @@ public final class HomeCard extends JPanel {
      *
      */
     public void setCard()  {
-        m_homeCard = new JPanel(new GridBagLayout());
-        m_homeCard.setPreferredSize(new Dimension(550, 520));
-        m_homeCard.setBorder(BorderFactory.createTitledBorder(BorderFactory
-                .createEtchedBorder(EtchedBorder.LOWERED), "Home"));
+            m_homeCard = new JPanel(new GridBagLayout());
+            m_homeCard.setPreferredSize(new Dimension(550, 520));
+            m_homeCard.setBorder(BorderFactory.createTitledBorder(BorderFactory
+                    .createEtchedBorder(EtchedBorder.LOWERED), "Home"));
+            try {
+                m_splashImage = ImageIO.read(new File("./images/nasdakTicker.jpg"));
+                JLabel imageLbl = new JLabel(new ImageIcon(m_splashImage));
 
-        //m_splashImage = ImageIO.read(new File("./images/naskaqImage.jpg"));
-        //JLabel imageLbl = new JLabel(new ImageIcon(m_splashImage));
-        JLabel name = new JLabel("Stock Ticker Portfolio Manager");
-        name.setFont(new Font("Serif", Font.PLAIN, 40));
+                m_constraints.gridx = 0;
+                m_constraints.gridy = 0;
+                m_constraints.fill = GridBagConstraints.NONE;
+                m_constraints.insets = new Insets(0, 0, 50, 0);
+                m_homeCard.add(imageLbl, m_constraints);
+            } 
+            catch (IOException ex) {
+                System.err.println("Exceptioin attempting to launch logo " + ex.getMessage());
+            }
 
-        m_constraints.gridx = 0;
-        m_constraints.gridy = 0;
-        m_constraints.fill = GridBagConstraints.NONE;
-        m_constraints.insets = new Insets(0, 0, 50, 0);
-        // m_homeCard.add(imageLbl, m_constraints);
+            JLabel name = new JLabel("Stock Ticker Portfolio Manager");
+            name.setFont(new Font("Serif", Font.PLAIN, 40));
 
-        m_constraints.gridx = 0;
-        m_constraints.gridy = 1;
-        m_constraints.insets = new Insets(0, 0, 100, 0);
-        m_homeCard.add(name, m_constraints);
+            m_constraints.gridx = 0;
+            m_constraints.gridy = 1;
+            m_constraints.insets = new Insets(0, 0, 100, 0);
+            m_homeCard.add(name, m_constraints);
     }
 
 
